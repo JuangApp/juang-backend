2e8ed3e1f609cf78307ec7b825a910e5c25f851a
kdmstj <rkddmstj429@gmail.com>
2023-02-01 21:45:32 +0900

    init: create project

---
+++ src/main/java/com/juang/gamnamoo/GamnamooApplication.java
@@ -0,0 +1,13 @@
+package com.juang.gamnamoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamnamooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamnamooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juang/gamnamoo/GamnamooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juang.gamnamoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamnamooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
